b5d30bcfc1dee3a7c7d37409914b47156d5a2938
Aazam Jutt <m.aazasultan@gmail.com>
2022-01-23 20:12:47 +0500

    uploaded files

---
+++ Youtube Api/Program.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Youtube_Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Playlist ID: ");
+            string playlistid = Console.ReadLine();
+            var result = GetVideoInPlaylistAsync(playlistid).Result;
+
+            PrintResult(result);
+
+            Console.ReadKey();
+
+        }
+
+        private static void PrintResult(dynamic result)
+        {
+            var count = result.items.Count;
+            var i = 0;
+
+            if (count > 0)
+                foreach (var item in result.items)
+                    Console.WriteLine(string.Format($"{++i,3}) {item.snippet.title}"));
+        }
+
+        private static async Task<dynamic> GetVideoInPlaylistAsync(string playlistid)
+        {
+            var parameter = new Dictionary<string, string>
+            {
+                ["key"] = ConfigurationManager.AppSettings["ApiKey"],
+                ["playlistId"] = playlistid,
+                ["part"] = "snippet",
+                ["fields"] = "items/snippet(title,description)",
+                ["maxResults"] = "50"
+            };
+
+            string baseUrl = "https://www.googleapis.com/youtube/v3/playlistItems?";
+            string fullUrl = MakeURLfromQuery(baseUrl, parameter);
+
+            var result = await new HttpClient().GetStringAsync(fullUrl);
+
+            if (result != null)
+            {
+                return JsonConvert.DeserializeObject(result);
+            }
+            return null;
+        }
+
+        private static string MakeURLfromQuery(string baseUrl, Dictionary<string, string> parameter)
+        {
+            if (string.IsNullOrEmpty(baseUrl))
+                throw new ArgumentNullException(nameof(baseUrl));
+
+            if (parameter == null || parameter.Count() == 0)
+                return baseUrl;
+
+            return parameter.Aggregate(baseUrl,
+                (accumulated, kvp) => string.Format($"{accumulated}{kvp.Key}={kvp.Value}&"));
+        }
+    }
+}
---
+++ Youtube Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Youtube Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Youtube Api")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5c1a44f-70f4-45db-a597-54aa5a872201")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
